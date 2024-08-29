1d0c6bfdc0c062fffb04009c84e7f9fc0168e3c9
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2024-08-29 15:33:24 +0200

    gradle

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("java")
+    id("io.papermc.paperweight.userdev") version "1.7.1" // Check for new versions at https://plugins.gradle.org/plugin/io.papermc.paperweight.userdev
+}
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+
+    flatDir {
+        dirs("lib")
+    }
+}
+
+dependencies {
+    //compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
+    paperweight.paperDevBundle("1.21.1-R0.1-SNAPSHOT")
+    implementation(files("lib/Movecraft-sign-rework-testbuild.jar"))
+}
+
+java.toolchain.languageVersion = JavaLanguageVersion.of(21)
---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+rootProject.name = "SquadronsReloadedReloaded"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://repo.papermc.io/repository/maven-public/")
+    }
+}
