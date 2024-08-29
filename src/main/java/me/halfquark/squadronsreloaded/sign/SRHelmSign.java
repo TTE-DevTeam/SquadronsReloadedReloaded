c8d9c42d6363b609242e1091963b23f2775e9e7d
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2024-08-29 16:06:05 +0200

    migrate helm sign

--- src/main/java/me/halfquark/squadronsreloaded/sign/SRHelmSign.java
+++ src/main/java/me/halfquark/squadronsreloaded/sign/SRHelmSign.java
@@ -1,82 +1,66 @@
 package me.halfquark.squadronsreloaded.sign;
 
-import org.bukkit.ChatColor;
-import org.bukkit.Tag;
-import org.bukkit.block.Block;
-import org.bukkit.block.Sign;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
-import org.bukkit.event.player.PlayerInteractEvent;
-
 import me.halfquark.squadronsreloaded.squadron.Squadron;
 import me.halfquark.squadronsreloaded.squadron.SquadronCraft;
-import me.halfquark.squadronsreloaded.squadron.SquadronManager;
-import net.countercraft.movecraft.craft.type.CraftType;
 import net.countercraft.movecraft.MovecraftLocation;
 import net.countercraft.movecraft.MovecraftRotation;
-import net.countercraft.movecraft.localisation.I18nSupport;
+import net.countercraft.movecraft.craft.Craft;
+import net.countercraft.movecraft.craft.type.CraftType;
+import net.countercraft.movecraft.sign.AbstractSignListener;
+import net.countercraft.movecraft.sign.HelmSign;
 import net.countercraft.movecraft.util.MathUtils;
+import org.bukkit.entity.Player;
+import org.bukkit.event.block.Action;
+import org.jetbrains.annotations.Nullable;
 
-public class SRHelmSign implements Listener {
+public class SRHelmSign extends HelmSign {
 
-	@EventHandler
-    public final void onSignClick(PlayerInteractEvent event) {
-        MovecraftRotation rotation;
-        if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-            rotation = MovecraftRotation.CLOCKWISE;
-        }else if(event.getAction() == Action.LEFT_CLICK_BLOCK){
-            rotation = MovecraftRotation.ANTICLOCKWISE;
-        }else{
-            return;
-        }
-        Block block = event.getClickedBlock();
-        if (!Tag.SIGNS.isTagged(block.getType())){
-            return;
-        }
-        Sign sign = (Sign) event.getClickedBlock().getState();
-        if (!(ChatColor.stripColor(sign.getLine(0)).equals("\\  ||  /") &&
-                ChatColor.stripColor(sign.getLine(1)).equals("==      ==") &&
-                ChatColor.stripColor(sign.getLine(2)).equals("/  ||  \\"))) {
-            return;
-        }
-        
-        Squadron sq = SquadronManager.getInstance().getPlayerSquadron(event.getPlayer(), true);
-		if(sq == null)
-			return;
-        
-		boolean onBoardCraft = false;
-	    for(SquadronCraft craft : sq.getCrafts()) {
-	    	if (MathUtils.locationNearHitBox(craft.getHitBox(),event.getPlayer().getLocation(),2)) {
-	    		onBoardCraft = true;
-		        break;
-		    }
-	    }
-	    if(!onBoardCraft)
-	    	return;
-		
-	    for(SquadronCraft craft : sq.getCrafts()) {
-	        if (!event.getPlayer().hasPermission("movecraft." + craft.getType().getStringProperty(CraftType.NAME) + ".rotate")) {
-	            event.getPlayer().sendMessage(I18nSupport.getInternationalisedString("Insufficient Permissions"));
-	            continue;
-	        }
-	        MovecraftLocation mLoc;
-	        if (craft.getType().getBoolProperty(CraftType.ROTATE_AT_MIDPOINT)) {
-	            mLoc = craft.getHitBox().getMidPoint();
-	        } else {
-	        	mLoc = MathUtils.bukkit2MovecraftLoc(sign.getLocation());
-	        }
-	        craft.rotate(rotation, mLoc, true);
-	        /*new BukkitRunnable() {
-				@Override
-				public void run() {
-					craft.rotate(rotation, mLoc);
+	@Override
+	protected boolean canPlayerUseSignOn(Player player, @Nullable Craft craft) {
+		if (!super.canPlayerUseSignOn(player, craft)) {
+			return false;
+		}
+		if (craft instanceof SquadronCraft sc) {
+			return sc.getSquadron().getPilot() == player;
+		}
+		return true;
+	}
+
+	@Override
+	protected boolean internalProcessSignWithCraft(Action clickType, AbstractSignListener.SignWrapper sign, Craft craft, Player player) {
+		if (craft instanceof SquadronCraft sc) {
+			MovecraftRotation rotation = MovecraftRotation.fromAction(clickType);
+
+			if (!MathUtils.locIsNearCraftFast(craft, MathUtils.bukkit2MovecraftLoc(player.getLocation()))) {
+				return false;
+			} else {
+				boolean onBoardCraft = false;
+				final Squadron squadron = sc.getSquadron();
+				for(SquadronCraft scTmp : squadron.getCrafts()) {
+					if (MathUtils.locationNearHitBox(craft.getHitBox(),player.getLocation(),2)) {
+						onBoardCraft = true;
+						break;
+					}
 				}
-	        }.runTaskLater(SquadronsReloaded.getInstance(), sq.getCraftRank(craft));*/
-	    }
-	    
-        event.setCancelled(true);
+				if(!onBoardCraft)
+					return false;
 
-    }
-	
+				final MovecraftLocation signLoc = MathUtils.bukkit2MovecraftLoc(sign.block().getLocation());
+				for(SquadronCraft scTmp : squadron.getCrafts()) {
+					MovecraftLocation mLoc;
+					if (craft.getType().getBoolProperty(CraftType.ROTATE_AT_MIDPOINT)) {
+						mLoc = craft.getHitBox().getMidPoint();
+					} else {
+						mLoc = signLoc;
+					}
+					scTmp.rotate(rotation, mLoc, true);
+				}
+
+				return true;
+			}
+		}
+		else {
+			return super.internalProcessSignWithCraft(clickType, sign, craft, player);
+		}
+	}
 }
