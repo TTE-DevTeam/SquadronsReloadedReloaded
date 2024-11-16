f045530f9e194fd3c5bc6e4179e60290b24d7ddc
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2024-11-16 14:26:33 +0100

    implement listener

--- src/main/java/me/halfquark/squadronsreloaded/listener/craft/TypeReloadListener.java
+++ src/main/java/me/halfquark/squadronsreloaded/listener/craft/TypeReloadListener.java
@@ -1,2 +1,18 @@
-package me.halfquark.squadronsreloaded.listener.craft;public class TypeReloadListener {
+package me.halfquark.squadronsreloaded.listener.craft;
+
+import me.halfquark.squadronsreloaded.sign.SRCraftPilotSign;
+import net.countercraft.movecraft.craft.CraftManager;
+import net.countercraft.movecraft.events.TypesReloadedEvent;
+import net.countercraft.movecraft.sign.MovecraftSignRegistry;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+
+public class TypeReloadListener implements Listener {
+
+    @EventHandler(ignoreCancelled = false, priority = EventPriority.LOWEST)
+    public void onCraftTypeReload(final TypesReloadedEvent event) {
+        MovecraftSignRegistry.INSTANCE.registerCraftPilotSigns(CraftManager.getInstance().getCraftTypes(), SRCraftPilotSign::new);
+    }
+
 }
