c3757ad18b7d101c7725a63991988363cb9bc48e
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2024-11-22 19:01:56 +0100

    respect lead craft size too

--- src/main/java/me/halfquark/squadronsreloaded/formation/FormationFormer.java
+++ src/main/java/me/halfquark/squadronsreloaded/formation/FormationFormer.java
@@ -18,17 +18,22 @@ public class FormationFormer {
 		CruiseDirection cd = sq.getDirection();
 		if(cd == null)
 			cd = CruiseDirection.NORTH;
+
 		Double x = (double) sq.getLeadCraft().getHitBox().getMidPoint().getX();
 		Double y = (double) sq.getLeadCraft().getHitBox().getMidPoint().getY();
 		Double z = (double) sq.getLeadCraft().getHitBox().getMidPoint().getZ();
+
+		final int leadCraftRadiusX = (sq.getLeadCraft().getHitBox().getMaxX() - craft.getHitBox().getMinX()) / 2;
+		final int leadCraftRadiusY = (sq.getLeadCraft().getHitBox().getMaxY() - craft.getHitBox().getMinY()) / 2;
+		final int leadCraftRadiusZ = (sq.getLeadCraft().getHitBox().getMaxZ() - craft.getHitBox().getMinZ()) / 2;
 
 		final int craftRadiusX = (craft.getHitBox().getMaxX() - craft.getHitBox().getMinX()) / 2;
 		final int craftRadiusY = (craft.getHitBox().getMaxY() - craft.getHitBox().getMinY()) / 2;
 		final int craftRadiusZ = (craft.getHitBox().getMaxZ() - craft.getHitBox().getMinZ()) / 2;
 
-		x += formation.getXPosition(sq.getCraftRank(craft), sq.getSpacing() + craftRadiusX, cd);
-		y += formation.getYPosition(sq.getCraftRank(craft), sq.getSpacing() + craftRadiusY, cd);
-		z += formation.getZPosition(sq.getCraftRank(craft), sq.getSpacing() + craftRadiusZ, cd);
+		x += formation.getXPosition(sq.getCraftRank(craft), sq.getSpacing() + craftRadiusX + leadCraftRadiusX, cd);
+		y += formation.getYPosition(sq.getCraftRank(craft), sq.getSpacing() + craftRadiusY + leadCraftRadiusY, cd);
+		z += formation.getZPosition(sq.getCraftRank(craft), sq.getSpacing() + craftRadiusZ + leadCraftRadiusZ, cd);
 		MovecraftLocation mLoc = craft.getHitBox().getMidPoint();
 		if((x-mLoc.getX())*(x-mLoc.getX()) + (y-mLoc.getY())*(y-mLoc.getY()) + (z-mLoc.getZ())*(z-mLoc.getZ()) <= Math.pow(SquadronsReloaded.FORMATIONROUNDDISTANCE, 2))
 			return;
